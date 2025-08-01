3782c966ec0c5cc7151b3fe1d5e7a9cd9f2805bf
mayar4ki <chrollo4ki@gmail.com>
2025-08-01 15:02:27 +0400

    ai pred phase 1 implementation

--- contracts/AiPredictionV1.sol
+++ contracts/AiPredictionV1.sol
@@ -2,29 +2,115 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 
-contract AiPredictionV1 {
+contract AiPredictionV1 is ReentrancyGuard {
 
-    address public adminAddress; // address of the admin
-    address public operatorAddress; // address of the operator
+    address public betMaker; //  admin address
+    uint256 public deadline; // bet deadline
 
-    uint256 public minBetAmount; // minimum betting amount (denominated in wei)
-    uint256 public treasuryFee; // treasury rate (e.g. 200 = 2%, 150 = 1.50%)
-    uint256 public treasuryAmount; // treasury amount that was not claimed
+    string public question;
 
-    mapping(uint256 => mapping(address => BetInfo)) public ledger;
+    uint256 public minBetAmount; // min betting amount (wei)
+    uint256 public betMakerShare; // treasury rate (e.g. 10,000 = 100%, 200 = 2%, 150 = 1.50%)
+    uint256 public betMakerShareAmount; // betMaker bet cut that was not claimed
 
-     enum BetOptions {
+
+    uint256 public constant BET_MAKER_MAX_SHARE = 1000; // 10%
+
+    uint256 public totalYesAmount;
+    uint256 public totalNoAmount;
+    uint256 public totalAmount;
+
+
+    mapping(address => BetInfo) public ledger;
+
+    enum BetOptions {
         Yes,
         No
-      }
+    }
     
     struct BetInfo {
-        BetOptions betOn;
+        BetOptions betOption;
         uint256 amount;
         bool claimed; // default false
     }
 
+
+    constructor(
+    string memory _question,
+    uint256 _betMakerShare,
+    uint256 _durationInHours,
+    uint256 _minBetAmount
+    ){
+         require(_betMakerShare <= BET_MAKER_MAX_SHARE, "admin share is too high");
+
+         question = _question;
+         betMakerShare = _betMakerShare;
+         deadline = block.timestamp + (_durationInHours * 1 hours);
+         minBetAmount = _minBetAmount;
+         betMaker = msg.sender;
+    }
+
+
+    function betYes() external payable nonReentrant notContract {
+        require(msg.value >= minBetAmount, "Bet amount must be greater than minBetAmount");
+        require(ledger[msg.sender].amount == 0, "Can only bet once per round");
+        require(block.timestamp <= deadline, "Betting period has ended");
+
+        ledger[msg.sender] = BetInfo({
+            betOption: BetOptions.Yes,
+            amount: msg.value,
+            claimed: false
+        });
+
+        totalYesAmount = totalYesAmount + msg.value;
+        totalAmount = totalAmount + msg.value;
+    }
+
+    function betNo() external payable nonReentrant notContract {
+        require(msg.value >= minBetAmount, "Bet amount must be greater than minBetAmount");
+        require(ledger[msg.sender].amount == 0, "Can only bet once per round");
+        require(block.timestamp <= deadline, "Betting period has ended");
+
+        ledger[msg.sender] = BetInfo({
+            betOption: BetOptions.No,
+            amount: msg.value,
+            claimed: false
+        });
+
+        totalNoAmount = totalNoAmount + msg.value;
+        totalAmount = totalAmount + msg.value;
+    }
+
+
+    function claim() external nonReentrant notContract {
+        require(block.timestamp > deadline, "Betting period has not ended yet");
+        require(ledger[msg.sender].amount > 0, "You have not bet yet");
+        require(!ledger[msg.sender].claimed, "You have already claimed your reward");
+
+            
+    }
+
+  
+
+    modifier notContract() {
+        require(!_isContract(msg.sender), "Contract not allowed");
+        require(msg.sender == tx.origin, "Proxy contract not allowed");
+        _;
+    }
+
+    /**
+     * @notice Returns true if `account` is a contract.
+     * @param account: account address
+     */
+    function _isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
 
 }
