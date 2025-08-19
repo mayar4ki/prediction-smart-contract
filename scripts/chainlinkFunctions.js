52767b259aa267226efd5916cd374fc5f246747a
mayar-deeb-xi <mayar.e.deeb@gmail.com>
2025-08-19 22:29:17 +0400

    feat add js

--- scripts/chainlinkFunctions.js
+++ scripts/chainlinkFunctions.js
@@ -1,38 +1,39 @@
-
-
-const question = args[0];
-const date = args[1];
+const question = args[0]
+const date = args[1]
 
 const payload = `
-question: "${prompt}"  
-Prediction Date: "${date}"
-`;
-
+question: '${prompt}'  
+Prediction Date: '${date}'
+`
 const apiResponse = await Functions.makeHttpRequest({
-  url: "https://api.openai.com/v1/responses",
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${secrets.openaiKey}`
-  },
-  method: "POST",
-  data: {
-    "prompt": {
-      "id": "pmpt_689e252d9ba081948c781705febb11a205cedd72e412f82a",
-      "version": "4"
-    }
-  }
-});
-
+    url: 'https://api.openai.com/v1/responses',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${secrets.openaiKey}`,
+    },
+    method: 'POST',
+    data: {
+        prompt: {
+            id: 'pmpt_689e252d9ba081948c781705febb11a205cedd72e412f82a',
+            version: '5',
+        },
+    },
+    timeout: 9000,
+})
 
 if (apiResponse.error || apiResponse?.data?.status !== 'completed') {
-  return Functions.encodeString("ERR:api")
+    return Functions.encodeString(apiResponse?.status ?? '500')
 }
 
 try {
-  const output = apiResponse?.data?.output?.filter(e => e.role === "assistant");
-  const tmp = JSON.parse(output?.[0]?.content?.[0]?.text ?? '{ "answer":"yes" }');
-  return Functions.encodeString(tmp.answer ?? "ERR:prop-404");
-}
-catch {
-  return Functions.encodeString(tmp.answer ?? "ERR:parse");
+    const output = apiResponse?.data?.output?.filter(
+        (e) => e.role === 'assistant'
+    )
+    const tmp = JSON.parse(output?.[0]?.content?.[0]?.text ?? '{}')
+    if (tmp?.answer) {
+        return Functions.encodeString(tmp?.answer.toUpperCase())
+    }
+    throw Error('400')
+} catch {
+    throw Error('400')
 }
