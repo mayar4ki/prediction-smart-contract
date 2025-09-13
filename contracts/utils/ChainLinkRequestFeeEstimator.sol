49a605c083becfafdf4b20d006ef8cc2e688271b
mayar-deeb-xi <mayar.e.deeb@gmail.com>
2025-09-14 01:02:34 +0400

    fix estimator

--- contracts/utils/ChainLinkRequestFeeEstimator.sol
+++ contracts/utils/ChainLinkRequestFeeEstimator.sol
@@ -6,26 +6,20 @@ pragma solidity >=0.8.2 <0.9.0;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 struct FunctionsCoordinatorConfig {
-    uint64 maxCallbackGasLimit;
-    uint32 gasOverhead;
-    uint32 requestTimeoutSeconds;
-    uint64 fulfillmentFlatFeeLinkPPMTier1;
-    uint64 fulfillmentFlatFeeLinkPPMTier2;
-    uint64 fulfillmentFlatFeeLinkPPMTier3;
-    uint64 fulfillmentFlatFeeLinkPPMTier4;
-    uint64 fulfillmentFlatFeeLinkPPMTier5;
-    uint24 reqsForTier2;
-    uint24 reqsForTier3;
-    uint24 reqsForTier4;
-    uint24 reqsForTier5;
-    address priceFeed;
+    uint16 maxConsumersPerSubscription; // ═════════╗ Maximum number of consumers which can be added to a single subscription. This bound ensures we are able to loop over all subscription consumers as needed, without exceeding gas limits. Should a user require more consumers, they can use multiple subscriptions.
+    uint72 adminFee; //                             ║ Flat fee (in Juels of LINK) that will be paid to the Router owner for operation of the network
+    bytes4 handleOracleFulfillmentSelector; //      ║ The function selector that is used when calling back to the Client contract
+    uint16 gasForCallExactCheck; // ════════════════╝ Used during calling back to the client. Ensures we have at least enough gas to be able to revert if gasAmount >  63//64*gas available.
+    uint32[] maxCallbackGasLimits; // ══════════════╸ List of max callback gas limits used by flag with GAS_FLAG_INDEX
+    uint16 subscriptionDepositMinimumRequests; //═══╗ Amount of requests that must be completed before the full subscription balance will be released when closing a subscription account.
+    uint72 subscriptionDepositJuels; // ════════════╝ Amount of subscription funds that are held as a deposit until Config.subscriptionDepositMinimumRequests are made using the subscription.
 }
 
 interface IFunctionsCoordinator {
     function getConfig() external view returns (FunctionsCoordinatorConfig memory);
 }
 
-abstract contract ChainLinkRequestFeeEstimator {
+contract ChainLinkRequestFeeEstimator {
     AggregatorV3Interface public immutable dataFeed;
     IFunctionsCoordinator public immutable coordinator;
 
@@ -41,71 +35,40 @@ abstract contract ChainLinkRequestFeeEstimator {
     /**
      * @notice Estimates the LINK fee for a request
      * @param callbackGasLimit The gas limit for the callback function
-     * @return linkFee The estimated LINK fee
+     * @return fee The estimated ETH fee
      */
-    function estimateLinkFee(uint256 callbackGasLimit) public view returns (uint256 linkFee) {
-        (, int256 price, , , ) = dataFeed.latestRoundData();
-        uint256 linkPerNative = uint256(price); // 1e18 format
+    function estimateFee(uint256 callbackGasLimit) public view returns (uint256 fee) {
 
-        uint64 fulfillmentFlatFeeLinkPPMTier1 = _getFulfillmentFlatFeeLinkPPMTier1();
-        uint32 gasOverhead = _getGasOverhead();
+        (, int256 price, , , ) = dataFeed.latestRoundData(); // 1. Fetch LINK/ETH price from Chainlink feed
+        uint256 linkPerEth = uint256(price); // 1e18 format
+
+        uint256 gasOverheadInJuels = _getRouterAdminFees();
+        uint256 gasOverheadInEth = (gasOverheadInJuels * linkPerEth) / 1e18; // 2. Convert LINK Juels to ETH
+        uint256 gasOverheadInGwei = gasOverheadInEth * 1e9; // 3. Convert ETH to Gwei
 
         uint256 baseFeeGwei = block.basefee / 1 gwei; // returns 30 Gwei
-        uint256 overestimatedGasPrice = (baseFeeGwei * (12)) / 10; // 20% buffer e.g. 36 Gwei
+        uint256 overestimatedGasPrice = (baseFeeGwei * 120) / 100; // 20% buffer e.g. 36 Gwei
 
         // 1 - Calculate the total gas cost (gwei): Gas price x (Gas overhead + Callback gas)
-        uint256 totalGasCost = overestimatedGasPrice * (gasOverhead + callbackGasLimit);
-
-        // 2 - Convert the gas cost to LINK using the LINK/ETH feed (e.g. 0.1 LINK/Gas) - result is in 1e18 format
-        uint256 gasCostInLink = (totalGasCost * 1e9) / linkPerNative;
+        uint256 totalGasCost = overestimatedGasPrice * (gasOverheadInGwei + callbackGasLimit);
 
         // 3 - The premium fee was converted from USD to LINK at the time of the request.
         // Add this converted premium fee to get the total cost of a request:
-        uint256 premiumFees = (fulfillmentFlatFeeLinkPPMTier1 * 1e12); // Convert PPM to LINK (1e6 PPM × 1e12 = 1e18) wei
+        uint32 fulfillmentGasPrice = _getFulfillmentGasPrice();
+        uint256 premiumFees = uint256(fulfillmentGasPrice);
 
-        uint256 totalRequestCost = gasCostInLink + premiumFees;
+        uint256 totalRequestCost = totalGasCost + premiumFees;
 
         return totalRequestCost;
     }
 
-    /**
-     * @notice Estimates the ETH fee for a request
-     * @param callbackGasLimit The gas limit for the callback function
-     * @return ethFee The estimated ETH fee
-     */
-    function estimateEtherFee(uint256 callbackGasLimit) public view returns (uint256 ethFee) {
-        (, int256 price, , , ) = dataFeed.latestRoundData();
-        uint256 linkPerNative = uint256(price); // 1e18 format
-
-        uint64 fulfillmentFlatFeeLinkPPMTier1 = _getFulfillmentFlatFeeLinkPPMTier1();
-        uint32 gasOverhead = _getGasOverhead();
-
-        uint256 baseFeeGwei = block.basefee / 1 gwei; // returns 30 Gwei
-        uint256 overestimatedGasPrice = (baseFeeGwei * (12)) / 10; // 20% buffer e.g. 36 Gwei
-
-        // 1 - Calculate the total gas cost (gwei): Gas price x (Gas overhead + Callback gas)
-        uint256 totalGasCost = overestimatedGasPrice * (gasOverhead + callbackGasLimit);
-
-        // 2 - Convert the gas cost to native wei - result is in 1e18 format
-        uint256 gasCostInETH = totalGasCost * 1e9;
-
-        // 3 - The premium fee was converted from USD to LINK at the time of the request.
-        // Add this converted premium fee to get the total cost of a request:
-        uint256 premiumFeesInLINK = (fulfillmentFlatFeeLinkPPMTier1 * 1e12); // Convert PPM to LINK (1e6 PPM × 1e12 = 1e18) wei
-        uint256 premiumFeesInETH = (premiumFeesInLINK * 1e8) / linkPerNative; // ETH in wei
-
-        uint256 totalRequestCost = gasCostInETH + premiumFeesInETH;
-
-        return totalRequestCost;
+    function _getFulfillmentGasPrice() private view returns (uint32) {
+        FunctionsCoordinatorConfig memory res = coordinator.getConfig();
+        return res.maxCallbackGasLimits[res.maxCallbackGasLimits.length - 1];
     }
 
-    function _getFulfillmentFlatFeeLinkPPMTier1() private view returns (uint64) {
+    function _getRouterAdminFees() private view returns (uint72) {
         FunctionsCoordinatorConfig memory res = coordinator.getConfig();
-        return res.fulfillmentFlatFeeLinkPPMTier1;
-    }
-
-    function _getGasOverhead() private view returns (uint32) {
-        FunctionsCoordinatorConfig memory res = coordinator.getConfig();
-        return res.gasOverhead;
+        return res.adminFee;
     }
 }
