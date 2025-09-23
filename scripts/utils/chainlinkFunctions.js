a1a557f69eb4b36ecb4c9083243ddaafdcb3e331
Mayar Deeb <mayar.e.deeb@gmail.com>
2025-09-23 18:36:34 +0400

    fix: сука

--- scripts/utils/chainlinkFunctions.js
+++ scripts/utils/chainlinkFunctions.js
@@ -1,39 +1,31 @@
-const question = args[0]
-const date = args[1]
-
-const payload = `
-question: '${prompt}'  
-Prediction Date: '${date}'
-`
 const apiResponse = await Functions.makeHttpRequest({
-    url: 'https://api.openai.com/v1/responses',
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${secrets.openaiKey}`,
+  url: "https://api.openai.com/v1/responses",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${secrets.openaiKey}`,
+  },
+  method: "POST",
+  data: {
+    prompt: {
+      id: "pmpt_689e252d9ba081948c781705febb11a205cedd72e412f82a",
+      version: "5",
     },
-    method: 'POST',
-    data: {
-        prompt: {
-            id: 'pmpt_689e252d9ba081948c781705febb11a205cedd72e412f82a',
-            version: '5',
-        },
-    },
-    timeout: 9000,
-})
-
-if (apiResponse.error || apiResponse?.data?.status !== 'completed') {
-    return Functions.encodeString(apiResponse?.status ?? '500')
+    input: [
+      { role: "user", content: `question: '${args[0]}', Date: '${args[1]}'` },
+    ],
+  },
+  timeout: 9000,
+});
+if (apiResponse.error) {
+  throw Error(apiResponse.status);
 }
-
 try {
-    const output = apiResponse?.data?.output?.filter(
-        (e) => e.role === 'assistant'
-    )
-    const tmp = JSON.parse(output?.[0]?.content?.[0]?.text ?? '{}')
-    if (tmp?.answer) {
-        return Functions.encodeString(tmp?.answer.toUpperCase())
-    }
-    throw Error('400')
+  const output = apiResponse?.data?.output?.filter(
+    (e) => e.role === "assistant"
+  );
+  return Functions.encodeString(
+    JSON.parse(output[0].content[0].text).answer.toUpperCase()
+  );
 } catch {
-    throw Error('400')
+  throw Error("parse");
 }
